863103a4511f693a248e87f88df7b477e85c7cc1
Yee Mon Aung <yeemonaung.isure@icloud.com>
2026-04-13 10:36:05 +0100

    Instrction and direction enum added

---
+++ ENUM/Direction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.ENUM
+{
+    public enum Direction
+    {
+        North,
+        East,
+        South,
+        West
+    }
+}
