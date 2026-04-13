061496bfb02abea30cf1317a21ae122e28077cc6
Yee Mon Aung <yeemonaung.isure@icloud.com>
2026-04-13 11:26:34 +0100

    Added Models folder for enum and data classes

---
+++ Models/PlateauSize.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    internal class PlateauSize
+    {
+        public int MaxX { get; set; }
+        public int MaxY { get; set; }
+
+        public PlateauSize(int maxX, int maxY)
+        {
+            MaxX = maxX;
+            MaxY = maxY;
+        }
+
+        public bool IsWithinBounds(Position position)
+        {
+            return position.X >= 0 && position.X <= MaxX && position.Y >= 0 && position.Y <= MaxY;
+        }
+    }
+}
