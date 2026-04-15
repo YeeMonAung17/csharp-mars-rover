7b30aad2c24b2ca7f6674597d0b85f2508007c8a
Yee Mon Aung <yeemonaung.isure@icloud.com>
2026-04-15 11:06:00 +0100

    Integration test completed

--- Program.cs
+++ Program.cs
@@ -2,64 +2,9 @@
 
 namespace MarsRover
 {
-    public class Program
+    public static class Program
     {
-       public static void Main()
-        {
-            //string plateauSizeInput = "5 5";
-            //string positionInput = "1 2 N";
-            //string instructionsInput = "LMLMLMLMM";
-
-            //try
-            //{
-            //    Plateau plateauSize = PlateauParser.Parse(plateauSizeInput);
-            //    Position position = PositionParser.Parse(positionInput);
-            //    List<Instruction> instructions = InstructionParser.Parse(instructionsInput);
-
-            //    if (!plateauSize.IsWithinBounds(position))
-            //    {
-            //        Console.WriteLine("Initial position is out of bounds.");
-            //        return;
-            //    }
-
-            //    Console.WriteLine("--- Mission Control Loaded ---");
-            //    Console.WriteLine($"Plateau Size: {plateauSize.MaxX} {plateauSize.MaxY}");
-            //    Console.WriteLine($"Rover Start: {position.X} {position.Y} {position.Direction}");
-            //    Console.WriteLine($"Instructions Loaded: {instructions.Count} commands");
-
-            //}
-
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine($"Error: {ex.Message}");
-            //}
-
-
-            List<string> input = new List<string> {"5 5",
-                "1 2 N",
-                "LMLMLMLMM",
-                "3 3 E",
-                "MMRMMRMRRM"
-            };
-
-
-            try
-            {
-                List<string> results = RunMission(input);
-
-                Console.WriteLine("--- Final Rover Positions ---");
-                foreach (var result in results)
-                {
-                    Console.WriteLine(result);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Mission Failed: {ex.Message}");
-            }
-        }
-
-        
+         
         public static List<string> RunMission(List<string> input)
         {
             // 1. Validation
